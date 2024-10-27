b66f1dbfa1ff2474964492df82b1267172526b07
github-WojciechZaczek <wojciech.zaczek84@gmail.com>
2024-10-27 18:56:55 +0100

    Remove weather_API_key.py from tracking

---
+++ URLs.py
@@ -0,0 +1,8 @@
+""" Air Pollution URLs """
+AIR_POLLUTION_URL = 'http://api.openweathermap.org/data/2.5/air_pollution?'
+AIR_POLLUTION_HISTORY_URL = 'http://api.openweathermap.org/data/2.5/air_pollution/history?'
+''' Weather URLs'''
+WEATHER_CURRENT_URL = 'http://api.openweathermap.org/data/2.5/weather?'
+WEATHER_DAILY_FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily?'
+''' Geo URLs'''
+GEO_DIRECT_URL = 'http://api.openweathermap.org/geo/1.0/direct?'
