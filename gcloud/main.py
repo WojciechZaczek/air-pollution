4a13685849eee73b3353a6b93ac4fc7e8ad79768
github-WojciechZaczek <wojciech.zaczek84@gmail.com>
2025-05-02 18:29:57 +0200

    build: extract

--- gcloud/main.py
+++ gcloud/main.py
@@ -16,8 +16,37 @@ def fetch_openweather_data(request, context=None):
     extract_object = Extract(
         strategy=openweather_strategy
     )
-
+    data = extract_object.retrieve_data()
+    # stworzyc publisher client publisher = pubsub_v1.PublisherClient()
+    # wysłać dane za pomocą publisher.publish(topic_path, data)
     return str(extract_object.retrieve_data())
 
 
+
+
+'''
+from google.cloud import pubsub_v1
+from google.oauth2 import service_account
+
+# Ścieżka do pliku klucza konta usługowego
+key_path = "key/pubsub-demo-438714-f39390e5cf71.json"
+
+# Ustawienia klienta pubsub z uwierzytelnianiem
+credentials = service_account.Credentials.from_service_account_file(key_path)
+publisher = pubsub_v1.PublisherClient(credentials=credentials)
+
+# Ustawienia projektu i tematu
+project_id = "pubsub-demo-438714"
+topic_id = "pubsub-topic-demo"
+topic_path = publisher.topic_path(project_id, topic_id)
+
+def publish_message():
+    data = "Hello, world!"
+    data = data.encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    print(f"Published message ID: {future.result()}")
+
+if __name__ == "__main__":
+    publish_message
+'''
 
