5cd7b286de0b6e0a1ef2e011ec5dc2672ed4a4d0
github-WojciechZaczek <wojciech.zaczek84@gmail.com>
2025-07-23 19:14:27 +0200

    Add pub/sub

--- gcloud/main.py
+++ gcloud/main.py
@@ -1,6 +1,7 @@
 import functions_framework
 import sys
 import os
+from google.cloud import pubsub_v1
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # add gcloud_functions to system paths
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # add root folder to system paths
@@ -18,10 +19,38 @@ def fetch_openweather_data(request, context=None):
         strategy=openweather_strategy
     )
     data = extract_object.retrieve_data()
-    # stworzyc publisher client publisher = pubsub_v1.PublisherClient()
-    # wysłać dane za pomocą publisher.publish(topic_path, data)
-    return str(extract_object.retrieve_data())
+    data = data.encode("utf-8")
+    publisher = pubsub_v1.PublisherClient()
+    project_id = "corded-shadow-429909-b2"
+    topic_id = "airpolution-topic"
+    topic_path = publisher.topic_path(project_id, topic_id)
+    future = publisher.publish(topic_path, data)
+    print(f"Published message ID: {future.result()}")
 
+    # return str(extract_object.retrieve_data())
+
+
+# https://cloud.google.com/functions/docs/deploy
+@functions_framework.cloud_event
+def example_function(request, context=None):
+    print("Hello")
+
+
+'''
+  WORKFLOW : fetch_openweather_data() -> send data to topic -> automatyczna egzekucja funkcji example_funciton() -> print Hello
+  
+  ===========================
+  
+  1. Stworzenie topicu w GCP    
+  
+  2. Poprawienie funkcji fetch_openweather_data():
+    - wysyla `data` do topicu
+    
+  3. Deployment drugiej funkcji event based:
+    - zsynchronizowana z topic do ktorego fetch_openweather_data() wysyla dane
+  
+  4. Test calego workflow
+'''
 
 
 
